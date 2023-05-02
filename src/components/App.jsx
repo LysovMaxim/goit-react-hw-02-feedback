736aee87161234bb08d645cb6960b55393ee076a
LysovMaxim <max_maran@icloud.com>
2023-05-02 20:19:01 +0300

    Створив компонент Feedback та прописав логіку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Feedback } from './Feedback/Feedback';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Feedback />
     </div>
   );
 };
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,81 @@
+import { Component } from 'react';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncreaseGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handleIncreaseNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleIncreaseBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+
+    const total = values.reduce((previousValue, number) => {
+      return previousValue + number;
+    }, 0);
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+      const values = Object.values(this.state);
+
+     const total = values.reduce((previousValue, number) => {
+      return previousValue + number;
+    }, 0);
+      
+    return Math.round((this.state.good / total) * 100);
+
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h2>Please leavel feedback</h2>
+        <button type="button" onClick={this.handleIncreaseGood}>
+          good
+        </button>
+        <button type="button" onClick={this.handleIncreaseNeutral}>
+          neutral
+        </button>
+        <button type="button" onClick={this.handleIncreaseBad}>
+          bad
+        </button>
+        <h3>Statistics</h3>
+        <p>
+          Good:<span> {good}</span>
+        </p>
+        <p>
+          Neutral:<span> {neutral}</span>
+        </p>
+        <p>
+          Bad:<span> {bad}</span>
+        </p>
+        <p>
+          Total:<span> {this.countTotalFeedback()}</span>
+        </p>
+        <p>
+          Positive feedback:
+          <span> {this.countPositiveFeedbackPercentage()}%</span>
+        </p>
+      </>
+    );
+  }
+}
